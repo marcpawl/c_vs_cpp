f272afdf9e228c6d4476b32297e9b5818b1f593e
Marc Pawlowsky <marcpawl@gmail.com>
2018-02-19 21:53:07 -0500

    Wrap the replace code into a function. C++

--- cpp.cpp
+++ cpp.cpp
@@ -1,9 +1,38 @@
-const std::string s = "*A";
-const std::string t = "*A\n";
- 
-std::string::size_type n = 0;
-while ( ( n = chartDataString.find( s, n ) ) != std::string::npos )
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void 
+str_replace(std::string& target, const char needle, const char replacement)
 {
-    chartDataString.replace( n, s.size(), t );
-    n += t.size();
+  std::string::size_type n;
+  while ( ( n = target.find( needle) ) != std::string::npos )
+  {
+    target[n] = replacement;
+  }
 }
+
+
+
+int main(int argc, char** argv)
+{
+  std::string orig=
+ #include "data.txt"
+
+  const char needle=',';
+  const char replacement = '\n';
+
+  str_replace(orig, needle, replacement);
+
+#if 0
+  std::cout << orig;
+#endif
+
+  return 0;
+} 
+
